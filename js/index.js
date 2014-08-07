ffdabe5655a5e7b3ecd2b86b9f1f1f074c39bbc1
Djordje Dimitrijev <rasplinjac@gmail.com>
2014-08-07 16:06:49 +0200

    Commit

--- js/index.js
+++ js/index.js
@@ -26,7 +26,11 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        if (window.cordova) {   //actual app
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+        } else {    //debugging in desktop browser
+            this.onDeviceReady();
+        }
     },
     // deviceready Event Handler
     //
@@ -37,13 +41,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
+//
         console.log('Received Event: ' + id);
     }
 };
